096f526f638c8c47e1b786e005d9d857fa205b7f
Rayhan Sheikh <rsheikh201031@mscse.uiu.ac.bd>
2021-10-05 23:14:39 +0600

    almost finished

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Project Name : Excellence Academy
 * This website is based on e-learning platform
-* Here you can admit different courses if you want
-* By this website you can easily complete your exam preparation
+* Here you can enroll different courses if you want
+* You can easily complete your exam preparation
 
 Website Link: https://excellence-academy.netlify.app/
--- src/components/DemoCourses/DemoCourses.js
+++ src/components/DemoCourses/DemoCourses.js
@@ -7,38 +7,23 @@ const DemoCourses = (props) => {
     return (
         // grid card design for courses 
         <div className=''>
-             {/* <div className="col ">
-                    <div className="card h-100 card-element card-design ">
-                    <img src={image} className="card-img-top img-course mx-auto d-block" alt="..."/>
-                    <div className="card-body text-ele">
-                        <h3 className="card-title">{title}</h3>
-                        <p className="card-text"><b>Total Subject:</b> {subject}</p>
-                        <p className="card-text"><b>Total Live Class:</b> {liveClass}</p>
-                        <p className="card-text"><b>Total Class Duration:</b> {duration}</p>
-                        <p className="card-text"><b>Exam: </b>{exam}</p>
-                        
-                    </div>
-                    <div className="card-footer">
-                        <Button className='px-5 btn-design'>Explore Course</Button>
-                    </div>
-                    </div>
-                </div> */}
-                <Card className="card h-100 card-element card-design " style={{ width: '28rem' }}>
-  <Card.Img className="card-img-top img-course mx-auto d-block" variant="top" src={image} />
-  <Card.Body>
-    <Card.Title>{title}</Card.Title>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem><b>Total Subject:</b> {subject}</ListGroupItem>
-    <ListGroupItem><b>Total Live Class:</b> {liveClass}</ListGroupItem>
-    <ListGroupItem><b>Total Class Duration:</b> {duration}</ListGroupItem>
-    <ListGroupItem><b>Exam: </b>{exam}</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-  <Button className='px-5 btn-design'>Explore Course</Button>
-  
-  </Card.Body>
-</Card>
+            
+                <Card className="card h-100 card-element card-design ">
+                    <Card.Img className="card-img-top img-course mx-auto d-block" variant="top" src={image} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem><b>Total Subject:</b> {subject}</ListGroupItem>
+                        <ListGroupItem><b>Total Live Class:</b> {liveClass}</ListGroupItem>
+                        <ListGroupItem><b>Total Class Duration:</b> {duration}</ListGroupItem>
+                        <ListGroupItem><b>Exam: </b>{exam}</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                    <Button className='px-5 btn-design'>Explore Course</Button>
+                    
+                    </Card.Body>
+                </Card>
         </div>
     );
 };
